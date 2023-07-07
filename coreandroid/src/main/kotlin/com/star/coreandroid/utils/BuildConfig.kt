58b3ca2cf8c1a3deba9633557b28d77a0ceb6e46
gabriel-estrela <gabrielestrela92@gmail.com>
2023-07-07 15:39:31 -0300

    feature: add reference for debug mode

--- coreandroid/src/main/kotlin/com/star/coreandroid/utils/BuildConfig.kt
+++ coreandroid/src/main/kotlin/com/star/coreandroid/utils/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.star.coreandroid.utils
 import android.content.Context
 import android.content.pm.ApplicationInfo
 
-
+// https://medium.com/mobile-app-development-publication/checking-debug-build-the-right-way-d12da1098120
 private fun Context.isDebug() =
     (this.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0)
 
